c87e9c9e131cf45c4edb679089f98fe31e89dc66
shu0115 <s.matsumoto0115@gmail.com>
2013-02-27 19:35:00 +0900

    Update

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Rails4sample
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # For Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
